4c310b37ef924d9b6b98329fbc048083aff8e95e
Leyverson <leyversonolivera14@gmail.com>
2026-06-09 20:51:44 -0300

    feat: adiciona aula node módulos

---
+++ node/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA) // Exibe o objeto completo do móduloA
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB) // Exibe o objeto completo do móduloB
